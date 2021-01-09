2835b1f5ea44434679fda81654697df004817eb3
Mittal <devmittal@paypal.com>
2021-01-09 12:12:37 +0530

    end point util function

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
-
 const axios = require("axios");
 
-let config1 = {
-    headers: {
-        "x-rapidapi-key": "dc8ab90c3amsh21d6ac1a1d42cd2p1a4da3jsnabdc8672582e",
-        "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
-    }
-}
+var configuration = {
+  headers: {
+    "x-rapidapi-key": "dc8ab90c3amsh21d6ac1a1d42cd2p1a4da3jsnabdc8672582e",
+    "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
+  }
+};
 
-app.get("/get-news", (req, res)=>{
+const util = (baseUrl) => {
+  return new Promise((resolve, reject) => {
+    axios.get(baseUrl, configuration).then((data) => {
+      resolve(data.data);
+    });
+  });
+};
 
-    axios.get("http://ludoqueer.herokuapp.com/all", config1).then(data=>{
-        res.send(data.data);
-    })
-    
-})
+var getNewsBaseUrl =
+  "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/get-news?category=";
+var getAnalysisBaseUrl =
+  "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-analysis?symbol=";
 
-const port = process.env.PORT||3010;
-app.listen(port, ()=>{
-    console.log("connected");
-})
+app.get("/get-news", (req, res) => {
+  getNewsBaseUrl += req.query.symbol;
+  util(getNewsBaseUrl).then((data) => {
+    res.send(data);
+  });
+});
+
+app.get("/get-analysis", (req, res) => {
+  getAnalysisBaseUrl += req.query.symbol;
+  util(getAnalysisBaseUrl).then((data) => {
+    res.send(data);
+  });
+});
+
+const port = process.env.PORT || 3011;
+app.listen(port, () => {
+  console.log(`connected on port ${port}`);
+});
